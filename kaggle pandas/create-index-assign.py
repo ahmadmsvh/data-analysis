f46c702be785aa106e0a9a2724518fa69e5e212b
amad <ahmadmsvh@gmail.com>
2022-05-03 13:16:59 +0430

    2022 May 3

--- kaggle pandas/create-index-assign.py
+++ kaggle pandas/create-index-assign.py
@@ -5,7 +5,7 @@ a = pd.DataFrame({
     'Yes'   : [50,21],
     'No'    : [131,2]
 })
-print(a,'\n')
+
 
 
 a = pd.DataFrame({
@@ -14,7 +14,7 @@ a = pd.DataFrame({
     },
     index=['question','answer']
 )
-print(a,'\n')
+
 
 a = pd.DataFrame({
     'Ali'   : [50,21],
@@ -22,100 +22,85 @@ a = pd.DataFrame({
     },
     index=['Yes','No']
 )
-print(a,'\n')
+
 
 
 b = pd.Series([1,2,'ali',1.23], index=['age','weight','name','index'],name='baby')
-print(b,'\n')
 
-
-melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
-describe = melbourne_houses.describe()
+melbourne_houses = pd.read_csv("./melb_data.csv", index_col='Price')
+melbourne_houses = pd.read_csv("./melb_data.csv", index_col=4)
 shape = melbourne_houses.shape
+describe = melbourne_houses.describe()
 head = melbourne_houses.head()
-
-print(head,'\n')
 
 
 cols = melbourne_houses.columns
-print(cols,'\n')
 
 
-melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
+melbourne_houses = pd.read_csv("./melb_data.csv", index_col=13)
 col = melbourne_houses.Price
-print(col,'\n')
+
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv")
 col = melbourne_houses.Price
-print(col,'\n')
+
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses['Price']
-print(col,'\n')
 
-
-melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
-col = melbourne_houses['Price'][1]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
-col = melbourne_houses.iloc[1:4]
-print(col,'\n')
+rec = melbourne_houses['Price'][1]
+
+
+
+melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
+rec = melbourne_houses.iloc[1:4]
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.iloc[1:4,2:5]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.iloc[[1,3,76],[1,3,6]]
-print(col,'\n')
+
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv")
 col = melbourne_houses.loc[[1,3,76],['Address','Price']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv",index_col=1)
 col = melbourne_houses.loc['25 Bloomburg St':'49 Roberts Rd',['Price','Rooms']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv")
 melbourne_houses.set_index('Rooms')
 col = melbourne_houses.iloc[[1,3,76],[1,3,6]]
-print(col,'\n')
-
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses['Rooms'] == 2
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.loc[(melbourne_houses['Rooms'] == 2) & (melbourne_houses['Price'] > 978000.0),['Rooms','Price']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.loc[melbourne_houses['Rooms'].isin([2,3]),['Rooms','Price']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.loc[melbourne_houses['Rooms'].notnull(),['Rooms','Price']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
 col = melbourne_houses.loc[:,['Rooms','Price']]
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv("./melb_data.csv", index_col=1)
--- kaggle pandas/group-sort.py
+++ kaggle pandas/group-sort.py
@@ -1,39 +1,51 @@
 import pandas as pd
 
+melbourne_houses = pd.read_csv('./melb_data.csv')
+col = melbourne_houses.groupby('Rooms')
+# for i in col:
+#     print(i)
+
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.groupby('Rooms').Price
-for i in col:
-    print(i)
+# for i in col:
+#     print(i)
 
+melbourne_houses = pd.read_csv('./melb_data.csv')
+col = melbourne_houses.loc[:,['Rooms']].groupby('Rooms')
+# for i in col:
+#     print(i)
+
+melbourne_houses = pd.read_csv('./melb_data.csv')
+col = melbourne_houses.loc[:,['Rooms','Price']].groupby('Rooms')
+# for i in col:
+#     print(i)
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.groupby('Rooms').Price.mean()
-print(col,'\n')
 
+
+melbourne_houses = pd.read_csv('./melb_data.csv')
+col = melbourne_houses.groupby('Rooms').Price.count()
 
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.count()
-print(col,'\n')
 
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.groupby(['Rooms','Suburb']).Price
-for i in col:
-    print(i)
+# for i in col:
+#     print(i)
 
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.groupby(['Rooms','Suburb']).Price
 data = col.apply(lambda d: d.iloc[0])
-print(data)
 
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
 col = melbourne_houses.groupby(['Rooms','Suburb']).Price.agg([len,min,max])
-print(col,'\n')
-
 
 
 melbourne_houses = pd.read_csv('./melb_data.csv')
